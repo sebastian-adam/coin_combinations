a201c2c0a5bf8f2d78940555a32a5a7c4ae3b8e5
Noah Yasskin and Sebastian Prokuski <sebastian@>
2016-06-30 09:08:46 -0700

    add working method for counting quarters and dimes

--- lib/coin_combinations.rb
+++ lib/coin_combinations.rb
@@ -1,6 +1,10 @@
 class Fixnum
   define_method (:coin_combinations) do
+    coin_array = []
     amount = self.divmod(25) #amount [2, 16]
-    # amount = amount[1].divmod(10)
+    coin_array.push(amount[0])
+    amount = amount[1].divmod(10)
+    coin_array.push(amount[0])
+    return coin_array
   end
 end
--- spec/coin_combinations_spec.rb
+++ spec/coin_combinations_spec.rb
@@ -4,7 +4,10 @@ require ('coin_combinations.rb')
 describe('Fixnum#coin_combinations') do
 
   it("returns the greatest number of quarters for a given amount") do
-    expect(66.coin_combinations).to(eq([2, 16]))
+    expect(66.coin_combinations).to(include(2))
   end
 
+  it("returns the greatest number of quarters and dimes for a given amount") do
+    expect(66.coin_combinations).to(include(2, 1))
+  end
 end
